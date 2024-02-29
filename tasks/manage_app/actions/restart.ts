610ec64d2b8b5d3c498723cf0972c80d8e0cbebe
josejooj <josejooj.dev@gmail.com>
2024-02-29 18:19:10 -0300

    comando restart criado

--- tasks/manage_app/actions/restart.ts
+++ tasks/manage_app/actions/restart.ts
@@ -3,23 +3,23 @@ import { get_application } from '../../../interfaces/square';
 import build_message from '../build_message';
 
 const command: Task = {
-    name: "start",
+    name: "restart",
     handler: async (client, int, data) => {
 
         if (!int.isButton()) return;
 
         const message_data = build_message(data.app, data.created_in, 'starting');
-        
+
         await int.update(message_data)
-        await client.square_api.applications.get(data.app.id).then(app => app.start());
-        
+        await client.square_api.applications.get(data.app.id).then(app => app.restart());
+
         const updated_application = await get_application(data.app.id, client);
         const updated_message_data = build_message(updated_application!, Date.now());
 
         await int.editReply(updated_message_data)
 
         client.interactions.set(int.message.id, { app: updated_application, created_in: Date.now(), author_id: int.user.id });
-
+        
     }
 }
 
