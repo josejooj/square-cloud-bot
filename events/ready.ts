245e7e8041288341f4c2dfc11c4114cf2cb0b30d
josejooj <josejooj.dev@gmail.com>
2024-02-29 16:01:45 -0300

    add support url on ready

--- events/ready.ts
+++ events/ready.ts
@@ -3,10 +3,15 @@ import { DiscordEvent } from "../interfaces/discord";
 const event: DiscordEvent<"ready"> = {
     name: "ready",
     listener: (client) => {
-        console.log(`\x1b[32mEstou online como ${client.user.tag}\x1b[0m`);
+        console.log(`\x1b[32mEstou online como \x1b[34m${client.user.tag}\x1b[0m`);
         console.log(
-            `\x1b[32mVocê pode me adicionar a qualquer servidor via: \x1b[35m%s\x1b[0m`,
+            `\x1b[32mVocê pode me adicionar a qualquer servidor via: \x1b[36m%s\x1b[0m`,
             `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`
+        )
+
+        console.log(
+            `\x1b[32mSe tiver qualquer problema, pode enviar em \x1b[36m%s\x1b[0m`,
+            `https://github.com/josejooj/square-cloud-bot/issues`
         )
     }
 }
